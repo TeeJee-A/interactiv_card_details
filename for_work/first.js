953c08f657ba86382d0c89a6635932c79b08241b
Ayoub Taji <ataji@e1r11p4.1337.ma>
2023-06-24 02:00:40 +0100

    now is finish with javascript and this is the first project of javascript for me

--- for_work/first.js
+++ for_work/first.js
@@ -1,60 +1,76 @@
-// function confirm() {
-//     const numberofcard = document.getElementById('number');
-//     // const year = document.getElementById('year').value;
-//     // const cvc = document.getElementById('cvc').value;
-//     const num = /^\(?([0-9]{4})\)?[ . ]?([0-9]{4})[ . ]?([0-9]{4})[ . ]?([0-9]{4})$/g;
-    
-//     if (!num.test(numberofcard.value))
-//         numberofcard.style.borderColor = 'red';
-//     else
-//         numberofcard.style.borderColor = '';
-// }
+var gChecker;
 
-// function username() {
-//     const username = document.getElementById('name');
-//     const name = /^[a-zA-Z][a-zA-Z ]*$/;
-//     // /^[a-zA-Z][a-zA-Z\\s]+$/
-//     if (!name.test(username.value))
-//         username.style.borderColor = 'red';
-//     else
-//         username.style.borderColor = '';
-// }
-
-// function mounth() {
-//     const mounth = document.getElementById('mounth');
-//     const remounth = /\b\d{2}\b/;
-//     if (!remounth.test(mounth.value))
-//         mounth.style.borderColor = 'red';
-//     else
-//         mounth.style.borderColor = '';
-// }
-
-// function year() {
-//     const year = document.getElementById('year');
-//     const reyear = /\b\d{2}\b/;
-//     if (!reyear.test(year.value))
-//         year.style.borderColor = 'red';
-//     else
-//         year.style.borderColor = '';
-// }
-
-// function cvc() {
-//     const cvc = document.getElementById('cvc') || document.getElementById('year');
-//     const recvc = /\b\d{3}\b/;
-//     if (!recvc.test(cvc.value))
-//         cvc.style.borderColor = 'red';
-//     else
-//         cvc.style.borderColor = '';
-// }
-
-const form = document.querySelector('.form')
-
-form.onsubmit = function batata(e) {
-    e.preventDefault()
-    
-    const newForm = new FormData(form)
-    
-    const formDataObj = {};
-    newForm.forEach((value, key) => (formDataObj[key] = value));
-    console.log(formDataObj);
+function usernumber() {
+	const numberofcard = document.getElementById('number');
+	const num = /^\(?([0-9]{4})\)?[ . ]?([0-9]{4})[ . ]?([0-9]{4})[ . ]?([0-9]{4})$/g;
+	
+	if (!num.test(numberofcard.value)) {
+		numberofcard.style.borderColor = 'red';gChecker = false;}
+	else {
+		numberofcard.style.borderColor = '';gChecker = true;}
 }
+
+function username() {
+	const username = document.getElementById('name');
+	const name = /^[a-zA-Z][a-zA-Z ]*$/;
+	if (!name.test(username.value)) {
+		username.style.borderColor = 'red';gChecker = false;}
+	else {
+		username.style.borderColor = '';gChecker = true;}
+}
+
+function usermounth() {
+	const mounth = document.getElementById('mounth');
+	const remounth = /\b\d{2}\b/;
+	if (!remounth.test(mounth.value)) {
+		mounth.style.borderColor = 'red';gChecker = false;}
+	else {
+		mounth.style.borderColor = '';gChecker = true;}
+}
+
+function useryear() {
+	const year = document.getElementById('year');
+	const reyear = /\b\d{2}\b/;
+	if (!reyear.test(year.value)) {
+		year.style.borderColor = 'red';gChecker = false;}
+	else {
+		year.style.borderColor = '';gChecker = true;}
+}
+
+function usercvc() {
+	const cvc = document.getElementById('cvc');
+	const recvc = /\b\d{3}\b/;
+	if (!recvc.test(cvc.value)) {
+		cvc.style.borderColor = 'red';gChecker = false;}
+	else {
+		cvc.style.borderColor = '';gChecker = true;}
+}
+
+var form = document.getElementById('form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  if (gChecker == true) {
+  	Toastify({
+		text: "success",
+		backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
+		position: "right",
+		duration: 3000
+  	}).showToast();
+	}
+	if (gChecker == false) {
+		Toastify({
+			text: "unsuccess",
+			backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
+			position: "right",
+			duration: 3000
+		}).showToast();
+	}
+	if (cvc.value.length == 0 || year.value.length == 0 || mounth.value.length == 0 || username.value.length == 0 || numberofcard.value.length == 0) {
+		Toastify({
+		  text: "The input is empty",
+		  backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
+		  position: "right",
+		  duration: 3000
+		}).showToast();
+	}
+});
--- for_work/test.js
+++
@@ -1,7 +0,0 @@
-function is_numeric(str){
-    return /^\d+$/.test(str);
-}
-
-console.log(is_numeric("123"));
-console.log(is_numeric("klsdjf"));
-console.log(is_numeric("dfs12dsds"));
